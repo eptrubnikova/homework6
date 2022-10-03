59bf4220cd2b30c428d4a388add27a7e253bb6d0
eptrubnikova <eptrubnikova@gmail.com>
2022-10-03 13:29:21 +0600

    first commit

---
+++ src/cource1/Main.java
@@ -0,0 +1,13 @@
+package cource1;
+
+public class Main {
+    public static void main(String[] args) { task1(); }
+
+    public static void task1() {
+        System.out.println("hello");
+    }
+
+
+
+    }
+
