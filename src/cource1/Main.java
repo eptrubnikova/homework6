29cd71585e2c69995fb247bcfaa9947555f75acc
eptrubnikova <eptrubnikova@gmail.com>
2022-10-06 12:52:08 +0600

    задачи 1-3

--- src/cource1/Main.java
+++ src/cource1/Main.java
@@ -1,13 +1,28 @@
 package cource1;
 
 public class Main {
-    public static void main(String[] args) { task1(); }
+    public static void main(String[] args) {
 
-    public static void task1() {
-        System.out.println("hello");
+        System.out.println("Задача 1");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача 2");
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача 3");
+
+        for (int i = 1; i < 17; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Итерация цикла " + i);
+            }
+        }
+
+
     }
+}
 
-
-
-    }
 
